6c5bfa0601310bcea43539afe84db9d04f99ab56
mason-c-web <79883350+mason-c-web@users.noreply.github.com>
2023-07-15 11:59:38 -0700

    adding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [temp, setTemp] = useState();
+  const [pants, setPants] = useState(false)
+   function getUserInfo (){
+    getLocation()
+
+  return axios.get('https://api.open-meteo.com/v1/forecast?latitude=47.62&longitude=-122.36&current_weather=true&temperature_unit=fahrenheit').then(res=>{
+  console.log(res.data.current_weather.temperature)
+  return res.data.current_weather.temperature
+  })
+
+
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geolocation is not supported by this browser.")
+  }
+}
+
+function showPosition(position) {
+ console.log( "Latitude: " + position.coords.latitude +
+  "<br>Longitude: " + position.coords.longitude)
+}
+useEffect(()=>{
+   getUserInfo().then(data =>{
+     setTemp(data)
+   }
+    )
+
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">    <h1>DO you need pants.com</h1>
+
+      {temp>40?
+       <div>{temp}</div>
+       :
+       <div> you dont need  pants</div>
+      }
+
     </div>
   );
 }
-
+//
 export default App;
